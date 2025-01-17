a8977ab3a0cecb4a5bd60ac1008ef2bcd2e7854b
Om Datt Tripathi <aryantripathi1606@gmail.com>
2025-01-17 15:00:44 +0530

    Addition : code done

---
+++ Addition.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Your First Number : ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter Your second Number : ");
+        int num2 = sc.nextInt();
+        System.out.println("Enter Your Third Number : ");
+        int num3 = sc.nextInt();
+
+        System.out.println("Sum of your number is :"+(num1+num2+num3));
+    }
+}
